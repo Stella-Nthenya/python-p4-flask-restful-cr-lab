129d6070b60faa2d212cf7170fa17946e6d7f1e4
Stella Nthenya <stellaalogi@gmail.com>
2024-06-10 22:28:43 +0300

    added columns

--- server/models.py
+++ server/models.py
@@ -5,5 +5,9 @@ db = SQLAlchemy()
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
-
+    
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)  # name column
+    image = db.Column(db.String, nullable=False)  # image column
+    price = db.Column(db.Numeric(10, 2), nullable=False)  # price column
+
